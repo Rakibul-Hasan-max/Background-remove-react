c7cf17488a420f706f319e9d1e11df131a6dcdfc
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-08-17 19:11:26 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">Copyright © 2022 Rakibul Hasan, All rights reserved.</span>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
